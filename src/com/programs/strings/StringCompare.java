727c1603b04ccaacefd614b6fb9fd0bc2d516f9a
SAMSUNG <SAMSUNG@DESKTOP-DCQSSB5>
2019-02-04 00:43:45 +0530

    updating a stringh comparison programs

---
+++ src/com/programs/strings/StringCompare.java
@@ -0,0 +1,14 @@
+package com.programs.strings;
+
+public class StringCompare {
+	public static void main(String[] args) {
+		String s1="mohan";
+		String s2="mohan";
+		String s3= new String("mohan");
+		System.out.println(s1.equals(s2));
+		System.out.println(s1 == s2);
+	}
+
+
+}
+	
