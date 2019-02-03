79345e69cb4c9329d5f8e10d99604b80119f4038
SAMSUNG <SAMSUNG@DESKTOP-DCQSSB5>
2019-02-04 01:00:56 +0530

    COMMIT NEW CHANGE IN STRINGS

--- src/com/programs/strings/StringCompare.java
+++ src/com/programs/strings/StringCompare.java
@@ -6,7 +6,7 @@ public class StringCompare {
 		String s2="mohan";
 		String s3= new String("mohan");
 		System.out.println(s1.equals(s2));
-		System.out.println(s1 == s2);
+		System.out.println(s1 == s3);
 	}
 
 
