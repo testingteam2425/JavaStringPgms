a4319c9af2b32fcbc1254e3b78361a84ed5987e7
HP <HP@DESKTOP-KKJRIED>
2019-02-04 23:24:29 +0530

    Repeated words program commited by gandhi

---
+++ src/com/programs/strings/ReapeatedWordsCount.java
@@ -0,0 +1,32 @@
+package com.programs.strings;
+
+public class ReapeatedWordsCount
+{
+
+
+	public static void main(String[] args)
+	{
+		String input="Welcome to Java Session Session Session";	
+		String[] words=input.split(" ");	
+		int wrc=1;							
+
+		for(int i=0;i<words.length;i++)		
+		{
+			for(int j=i+1;j<words.length;j++)	
+			{
+
+				if(words[i].equals(words[j]))	
+				{
+					wrc=wrc+1;				
+					words[j]="0";			
+				}
+			}
+			if(words[i]!="0")
+				System.out.println(words[i]+"--"+wrc);
+			wrc=1;
+
+		}  
+
+	}
+
+}
